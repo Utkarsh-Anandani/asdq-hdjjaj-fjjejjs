a7978e46072bbaae573b77522c45f8d6c4aefe84
Utkarsh-Anandani <utkarshanandani9@gmail.com>
2025-03-30 16:13:19 +0530

    Added the output section of the app

---
+++ src/common/Table.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+function Table({ data }: { data: any[] }) {
+    const [queryResults, setQueryResults] = useState<any[]>([]);
+
+    useEffect(() => {
+        setQueryResults(data);
+    }, [data]);
+
+    return (
+        <div>
+            {queryResults.length > 0 ? (
+                <table className="table-component" border={1}>
+                    <thead>
+                        <tr>
+                            {Object.keys(queryResults[0]).map((key) => (
+                                <th key={key}>{key.toUpperCase()}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {queryResults.map((result, index) => (
+                            <tr key={index}>
+                                {Object.values(result).map((value, i) => (
+                                    <td key={i}>{String(value)}</td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : (
+                <p>No data available</p>
+            )}
+        </div>
+    );
+}
+
+export default Table;
--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,7 +1,15 @@
+import Table from "../common/Table";
+import ProductDetails from '../../public/products.json';
+
 function Output() {
     return(
         <div className="output">
-
+            <div className="output-header">
+                <div className="output-header-left">Output</div>
+            </div>
+            <div className="table-container">
+                <Table data={ProductDetails} />
+            </div>
         </div>
     );
 }
